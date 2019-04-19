49e5d7b719ab8f8bb7d53867617481d23048ebd3
danegailitis <danegailitis@gmail.com>
2019-04-19 15:36:41 -0400

    created basic node server file

---
+++ public/myserver.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var fs = require('fs');
+
+function onRequest(request,response)
+{
+    response.writeHead(200,{'Content-Type': 'text/html'});
+    fs.readFile('./personal.html',null,function(error,data){
+        if (error)
+        {
+            response.writeHead(404);
+            response.write('File Not Found');
+        }
+        else
+        {
+            response.write(data);
+        }
+        response.end();
+    });
+}
+http.createServer(onRequest).listen(8000);
