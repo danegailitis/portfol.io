925e2408312439d13ac4819a0184f1da6c43855e
danegailitis <danegailitis@gmail.com>
2019-04-20 09:37:36 -0400

    replaced old node server with express

--- public/myserver.js
+++ public/myserver.js
@@ -1,20 +1,27 @@
-var http = require('http');
-var fs = require('fs');
 
-function onRequest(request,response)
-{
-    response.writeHead(200,{'Content-Type': 'text/html'});
-    fs.readFile('./personal.html',null,function(error,data){
-        if (error)
-        {
-            response.writeHead(404);
-            response.write('File Not Found');
-        }
-        else
-        {
-            response.write(data);
-        }
-        response.end();
-    });
-}
-http.createServer(onRequest).listen(8000);
+var express = require("express");
+ var app = express();
+
+ /* serves main page */
+ app.get("/", function(req, res) {
+    res.sendFile('/Users/danegailitis/Projects/portfolio/public/index.html')
+ });
+
+  app.get("/profile",function(req,res){
+      res.sendFile("/Users/danegailitis/Projects/portfolio/public/personal.html");
+  });
+
+  app.get("/login",function(req,res){
+    res.sendFile("/Users/danegailitis/Projects/portfolio/public/login.html");
+});
+
+ /* serves all the static files */
+ app.get(/^(.+)$/, function(req, res){ 
+     console.log('static file request : ' + req.params);
+     res.sendFile( __dirname + req.params[0]); 
+ });
+
+ var port = process.env.PORT || 5000;
+ app.listen(port, function() {
+   console.log("Listening on " + port);
+ });
