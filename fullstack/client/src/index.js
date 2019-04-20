13e1a3deabec8051e14dbce716df56da64a12291
Zach Birenbaum <zbirenba@purdue.edu>
2019-04-20 01:18:02 -0400

    React Working. Skipping rest of fullstack until firebase integration

---
+++ fullstack/client/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LineChart from './LineChart';
+//import serviceWorker from './serviceWorker';
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register()
+        .then(function (reg) {
+            // registration worked
+            console.log('Registration succeeded. Scope is ' + reg.scope);
+        }).catch(function (error) {
+            // registration failed
+            console.log('Registration failed with ' + error);
+        });
+}
+
+
+ReactDOM.render(<LineChart />, document.getElementById('root'));
