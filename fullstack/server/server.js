0ae8edf38d032d26b6f8ff168f87768b19d6567b
Zach Birenbaum <zbirenba@purdue.edu>
2019-04-20 20:44:59 -0400

    del s2

--- fullstack/server/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 3001
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-});
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
