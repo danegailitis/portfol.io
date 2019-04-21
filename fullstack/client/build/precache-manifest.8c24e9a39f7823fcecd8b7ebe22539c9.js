c302befe0b35fd37a9418d2315d59bd6946a07a2
Zach Birenbaum <zbirenba@purdue.edu>
2019-04-20 21:10:00 -0400

    dd

---
+++ fullstack/client/build/precache-manifest.8c24e9a39f7823fcecd8b7ebe22539c9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "223889ae855cc94573a4",
+    "url": "/static/js/main.af5cacd5.chunk.js"
+  },
+  {
+    "revision": "4149c7dcda07c79570db",
+    "url": "/static/js/2.ff64b9b5.chunk.js"
+  },
+  {
+    "revision": "fa9a9686e020e1f6acc8608da62ed685",
+    "url": "/index.html"
+  }
+];
