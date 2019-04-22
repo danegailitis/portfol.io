18d30c724bc04aa222430f2b1db18f5f1c1f3163
danegailitis <danegailitis@gmail.com>
2019-04-22 10:59:53 -0400

    saving before branch

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,37 @@
 const functions = require('firebase-functions');
+const express = require("express");
+const path = require("path");
 
+const app = express();
+
+
+app.get("/", function(req, res) {
+    var mypath = path.join(__dirname+"/../");
+   res.sendFile(mypath+'/public/index.html');
+});
+
+ app.post("/profile",function(req,res){
+     var mypath = path.join(__dirname+"/../");
+     res.sendFile(mypath+"/public/personal.html");
+ });
+
+ app.get("/login",function(req,res){
+   var mypath = path.join(__dirname+"/../");
+   res.sendFile(mypath+"/public/login.html");
+});
+
+/* serves all the static files */
+app.get(/^(.+)$/, function(req, res){ 
+    console.log('static file request : ' + req.params);
+    res.sendFile( __dirname + req.params[0]); 
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
+exports.app = functions.https.onRequest(app);
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
