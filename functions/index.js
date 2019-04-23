b803595ee7ce763e152c0b96858bfa82f60c1a72
danegailitis <danegailitis@gmail.com>
2019-04-23 02:25:37 -0400

    debugging server

--- functions/index.js
+++ functions/index.js
@@ -1,41 +1,42 @@
 const functions = require('firebase-functions');
-const express = require("express");
-const path = require("path");
+var express = require("express");
+var path = require("path");
 
-const app = express();
+var app = express();
 
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
     console.log('event triggered for homepage');
     var mypath = path.join(__dirname+'/../');
     res.sendFile(mypath+'/index.html');
 });
 
-app.post('/', function(req, res) {
+/*app.post('/', (req, res) => {
     var mypath = path.join(__dirname+'/../');
    res.sendFile(mypath+'/index.html'); // Testing
-});
+});*/
 
- app.post('/profile',function(req,res){
+ app.post('/profile',(req,res) => {
     console.log('event triggered for profile');
     res.sendFile(__dirname+'/public/personal.html');
  });
 
- app.get('/login',function(req,res){
+ app.get('/login',(req,res) => {
     console.log('event triggered for login');
-    res.sendFile(__dirname+'/public/login.html');
+    const fullPath = path.normalize(__dirname + '/public/login.html');
+    res.sendFile(fullPath);
 });
 
 /* serves all the static files */
-app.get(/^(.+)$/, function(req, res){ 
+/*app.get(/^(.+)$/, function(req, res){ 
     console.log('static file request : ' + req.params);
     res.sendFile( __dirname + req.params[0]); 
-});
+});*/
 
-var port = process.env.PORT || 5000;
+/*var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log('Listening on ' + port);
-});
+});*/
 
 exports.app = functions.https.onRequest(app);
 // // Create and Deploy Your First Cloud Functions
